fa97b8b9efa1148f372251d8655676f1647f1492
Noname-29-lnin <128560496+Noname-29-lnin@users.noreply.github.com>
2025-12-03 15:08:38 +0700

    update slt

--- Version1/01_slt/lib/framework_custom.hpp
+++ Version1/01_slt/lib/framework_custom.hpp
@@ -16,14 +16,22 @@ using SIZE_TYPE_T   = DATATYPE_VAR;
 using SIZE_ARR_T    = DATATPPE_ARR;
 
 class C_RTL_Func{
+    private:
+        SIZE_TYPE_T F_Cal_Sum(bool start, std::vector<SIZE_ARR_T> &arr, int addr_si, int addr_ei);
+        SIZE_TYPE_T F_Cal_Div(bool start, SIZE_ARR_T i_dividend, SIZE_ARR_T i_divisor);
+        SIZE_TYPE_T F_Cal_divisor(bool start, int addr_si, int addr_ei);
     public:
-        SIZE_TYPE_T F_Cal_Mean(std::vector<SIZE_ARR_T> &arr, int si, int ei);
+        SIZE_TYPE_T F_Cal_Mean( std::vector<SIZE_ARR_T> &arr, int si, int ei);
 };
 
-// class C_RTL_Framework_Serial {
-//     private:
-//         Status_e P_status;
-//     public:
+class C_RTL_Framework_Serial {
+    private:
+        size_t P_count_swap     = 0;
+        size_t P_count_compare  = 0;
+        void RTL_Stack(bool i_pop, SIZE_ARR_T i_data, bool i_push, SIZE_ARR_T &o_data);
+        SIZE_TYPE_T RTL_Cal_Mean(std::vector<SIZE_ARR_T> &arr, int addr_si, int addr_ei);
+        SIZE_ARR_T RTL_partition(std::vector<SIZE_ARR_T> &arr, int addr_si, int addr_ei);
+    public:
 
-// };
+};
 #endif
--- Version1/01_slt/src/framework_custom.cpp
+++ Version1/01_slt/src/framework_custom.cpp
@@ -3,9 +3,37 @@
 /*
     C_RTL_Func
 */
-SIZE_TYPE_T C_RTL_Func::F_Cal_Mean(std::vector<SIZE_ARR_T> &arr, int si, int ei){
+
+SIZE_TYPE_T C_RTL_Func::F_Cal_Sum(bool start, std::vector<SIZE_ARR_T> &arr, int addr_si, int addr_ei){
+    SIZE_TYPE_T temp_sum = 0;
+    if(!start){
+        return temp_sum;
+    } else {
+        for(int i = addr_si; i <= addr_ei; i++){
+            temp_sum += arr[i];
+        }
+        return temp_sum;
+    }
+}
+SIZE_TYPE_T C_RTL_Func::F_Cal_Div(bool start, SIZE_ARR_T i_dividend, SIZE_ARR_T i_divisor){
+    if(!start){
+        return 1;
+    } else {
+        return i_dividend/i_divisor;
+    }
+}
+SIZE_TYPE_T C_RTL_Func::F_Cal_divisor(bool start, int addr_si, int addr_ei){
+    if(!start){
+        return 1;
+    } else {
+        return (SIZE_ARR_T) (addr_ei - addr_si + 1);
+    }
+}
+
+SIZE_TYPE_T C_RTL_Func::F_Cal_Mean(std::vector<SIZE_ARR_T> &arr, int si, int ei)
+{
     SIZE_TYPE_T t_sum = 0;
-    for(int i =si; i <= ei; i++){
+    for (int i = si; i <= ei; i++){
         t_sum += arr[i];
     }
     SIZE_TYPE_T t_div = 1;
@@ -13,3 +41,47 @@ SIZE_TYPE_T C_RTL_Func::F_Cal_Mean(std::vector<SIZE_ARR_T> &arr, int si, int ei)
     return t_div;
 }
 
+// Hirecture
+// |- Stack
+// |- CheckData
+
+void C_RTL_Framework_Serial::RTL_Stack(bool i_pop, SIZE_ARR_T i_data, bool i_push, SIZE_ARR_T &o_data)
+{
+    static std::vector<SIZE_ARR_T> stack_mem;
+    if (i_push)
+    {
+        stack_mem.push_back(i_data);
+    }
+    else if (i_pop && !stack_mem.empty())
+    {
+        o_data = stack_mem.back();
+        stack_mem.pop_back();
+    }
+}
+
+SIZE_TYPE_T C_RTL_Framework_Serial::RTL_Cal_Mean(std::vector<SIZE_ARR_T> &arr, int addr_si, int addr_ei)
+{
+    SIZE_TYPE_T temp_sum = 0;
+    SIZE_TYPE_T temp_div = 1;
+    for (int i = addr_si; i <= addr_ei; i++)
+    {
+        temp_sum += arr[i];
+    }
+    temp_div = temp_sum / (addr_ei - addr_si + 1);
+    return temp_div;
+}
+SIZE_ARR_T C_RTL_Framework_Serial::RTL_partition(std::vector<SIZE_ARR_T> &arr, int addr_si, int addr_ei)
+{
+    SIZE_TYPE_T bi = addr_si;
+    SIZE_TYPE_T mean = RTL_Cal_Mean(arr, addr_si, addr_ei);
+    for (int i = addr_si; i <= addr_ei; i++)
+    {
+        if (arr[i] < mean)
+        {
+            std::swap(arr[i], arr[bi]);
+            bi++;
+        }
+    }
+
+    return bi - 1;
+}
